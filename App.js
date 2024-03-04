e38fae660b4196f57c0e94b2fc32422c515e18d6
FarahnazMokhtari <100504557+FarahnazMokhtari@users.noreply.github.com>
2024-03-04 23:55:45 +0100

    job excersice

---
+++ App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Courses from "./courses";
+
+function App() {
+  return <Courses  />;
+}
+// const JobComponent = (prop) => {
+//   return (
+//     <div>
+//       <h1>{prop.salary}</h1>
+//       <h2>{prop.jobTitel}</h2>
+//       <h3>{prop.company}</h3>
+//     </div>
+//   );
+// };
+
+export default App;
