7ef1f9029aaa0411ccd2268ff37135a561b6f876
FarahnazMokhtari <100504557+FarahnazMokhtari@users.noreply.github.com>
2024-03-05 00:20:08 +0100

    up-down-0 button with useState

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import "./App.css";
-import Courses from "./courses";
+//import Courses from "./courses";
+import { useState } from "react";
 
-function App() {
-  return <Courses  />;
-}
+ function App() {
+  const [count , setCount]=useState(0);
+  return(
+    <div>
+      <button onClick={()=>setCount(count+1)}>increase</button>
+      <button onClick={()=>setCount(count-1)}>decrease</button>
+      <button onClick={()=>setCount(0)}>set to zero</button>
+      <h1>{count}</h1>
+    </div>
+  )
+ }
+//   return <Courses  />;
+// }
 // const JobComponent = (prop) => {
 //   return (
 //     <div>
